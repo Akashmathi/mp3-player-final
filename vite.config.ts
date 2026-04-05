130fa331a7da65b1f55f826ebbb9347297bd4f33
Akashmathi <akashmathi777@gmail.com>
2026-04-06 00:11:28 +0530

    Fix Error 427

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  define: {
-    'process.env': {},
-  },
   server: {
     host: true, // Allow external access (for mobile testing)
   },
